e962561e8366afb4cfe3a4ddfedaa4a152a58330
CALLUM HENDERSON <tankerok8@aol.com>
2018-04-26 16:23:29 +0100

    InitialCommit
    Signed-off-by: CALLUM HENDERSON <tankerok8@aol.com>

---
+++ InputCommands.h
@@ -0,0 +1,15 @@
+#pragma once
+
+struct InputCommands
+{
+	bool forward;
+	bool back;
+	bool right;
+	bool left;
+	bool rotRight;
+	bool rotLeft;
+	bool leftClick;
+	bool rightClick;
+	int mouseX;
+	int mouseY;
+};
